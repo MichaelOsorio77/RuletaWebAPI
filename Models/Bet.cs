4706db8278b56119172fb958c86b0d85c85e8f1a
Michael Osorio <mosorio@skitconsulting.com>
2021-01-11 14:20:51 -0500

    Creation of the data model to store the roulette information

---
+++ Models/Bet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuletaWebAPI.Models
+{
+    public class Bet
+    {
+        public int Id { get; set; }
+        public string PlayedValue { get; set; }
+        public double StakeValue { get; set; }
+        public string BetType { get; set; }
+        [ForeignKey("FK_Roulette")]
+        public int IdRoulette { get; set; }
+    }
+}
